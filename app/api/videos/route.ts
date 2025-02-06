9d1041a255018795ad3662e3e7bf520cc3231021
parasnever <parasshresthanever@gmail.com>
2025-02-07 00:05:43 +0545

    add POST route for video creation with authentication and validation

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,6 +1,10 @@
+import { authOptions } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
-import Video from "@/models/video";
-import { NextResponse } from "next/server";
+import Video, { IVideo } from "@/models/video";
+import { error } from "console";
+
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
@@ -13,6 +17,44 @@ export async function GET() {
   } catch (error) {
     return NextResponse.json(
       { error: "Failed to fetch videos" },
+      { status: 200 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    await connectToDatabase();
+    const body: IVideo = await request.json();
+    if (
+      !body.title ||
+      !body.description ||
+      !body.videoUrl ||
+      !body.thubmnailUrl
+    ) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+    const videoData = {
+      ...body,
+      controls: body.controls ?? true,
+      transformtion: {
+        height: 1920,
+        width: 1080,
+        quality: body.transformation?.quality ?? 100,
+      },
+    };
+    const newVidoo = Video.create(videoData);
+    return NextResponse.json(newVidoo);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to create a videos" },
       { status: 200 }
     );
   }
