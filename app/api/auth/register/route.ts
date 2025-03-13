76ffc7b92c9089e8eec8d8379d085084229a80c5
parasnever <parasshresthanever@gmail.com>
2025-03-14 03:13:25 +0545

    fix(Register): implement password hashing and improve error messages

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,6 +1,47 @@
+// import { NextRequest, NextResponse } from "next/server";
+// import { connectToDatabase } from "@/lib/db";
+// import User from "@/app/models/User";
+
+// export async function POST(req: NextRequest) {
+//   try {
+//     const { email, password } = await req.json();
+//     if (!email || !password) {
+//       return NextResponse.json(
+//         { error: "Email and password are required" },
+//         { status: 400 }
+//       );
+//     }
+
+//     await connectToDatabase();
+
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       return NextResponse.json(
+//         { error: "Email is already exist" },
+//         { status: 400 }
+//       );
+//     }
+
+//     await User.create({
+//       email,
+//       password,
+//     });
+//     return NextResponse.json(
+//       { message: "User registered successfully" },
+//       { status: 201 }
+//     );
+//   } catch (error) {
+//     console.error("Error:", error);
+//     return NextResponse.json(
+//       { error: "Failed to register User" },
+//       { status: 500 }
+//     );
+//   }
+// }
 import { NextRequest, NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/db";
 import User from "@/app/models/User";
+import bcrypt from "bcryptjs";
 
 export async function POST(req: NextRequest) {
   try {
@@ -17,15 +58,19 @@ export async function POST(req: NextRequest) {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return NextResponse.json(
-        { error: "Email is already exist" },
+        { error: "Email already in use" },
         { status: 400 }
       );
     }
 
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     await User.create({
       email,
-      password,
+      password: hashedPassword, // Store the hashed password
     });
+
     return NextResponse.json(
       { message: "User registered successfully" },
       { status: 201 }
