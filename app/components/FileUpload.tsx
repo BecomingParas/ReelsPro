ecf5bb7e683957a675d15bed50c6495bc2acd0d2
parasstha <parasstha.skillprompt@gmail.com>
2025-04-15 22:40:42 +0545

    changes for vercel

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -88,7 +88,7 @@ export default function FileUpload({
         </div>
       )}
 
-      {error && <div className="text-error text-sm">{error}</div>}
+      {error && <div className="text-error text-lg">{error}</div>}
     </div>
   );
 }
